d15857e2bdfa717cfc1723d077f877403ba5f97e
unknown <gcmadruga@.(none)>
2009-12-11 20:29:54 -0200

    deleted files we wont use anymore because its on grails 1.2 core merge from GrailsExecutionListener to GrailsEasybListener

--- src/groovy/grails/plugin/easyb/test/listener/GrailsEasybListener.groovy
+++ src/groovy/grails/plugin/easyb/test/listener/GrailsEasybListener.groovy
@@ -7,13 +7,18 @@ import org.easyb.result.Result
 import org.easyb.domain.Behavior
 import grails.plugin.easyb.test.report.EasybReportsFactory
 import org.easyb.BehaviorStep
+import org.codehaus.groovy.grails.test.plugin.easyb.InjectTestRunnerFactory
+import org.codehaus.groovy.grails.test.plugin.easyb.InjectTestRunner
 
 public class GrailsEasybListener extends ResultsCollector {
     final protected GrailsTestEventPublisher eventPublisher
     final protected EasybReportsFactory reportsFactory
     final protected SystemOutAndErrSwapper outAndErrSwapper
 
-    protected currentStep
+    protected Behavior currentBehaviour
+    protected Stack<BehaviorStep> steps = new Stack<BehaviorStep>();
+    protected BehaviorStep currentStep
+    protected InjectTestRunner testRunner
 
     GrailsEasybListener(GrailsTestEventPublisher eventPublisher, EasybReportsFactory reportsFactory, SystemOutAndErrSwapper outAndErrSwapper) {
         super()
@@ -26,34 +31,99 @@ public class GrailsEasybListener extends ResultsCollector {
      * Called when a Story or Specification file before being executed (comparable as a TestCase).
      * Publishes the event testCaseStart.
      */
+    @Override
     public void startBehavior(Behavior behavior) {
         super.startBehavior(behavior)
         eventPublisher.testCaseStart(trucateEventName(behavior.phrase))
+
+        currentBehaviour = behavior
+        testRunner = InjectTestRunnerFactory.findMatchingRunner(currentBehaviour, type)
+
+        if(! testRunner) {
+            testRunner = InjectTestRunnerFactory.getDefault(currentBehaviour, type)
+        }
+
+        if(testRunner) {
+            testRunner.initialize()
+
+            if (testRunner.testCase == null) {
+                log.warn "Unable to create expected test runner ${testRunner.runnerType}, using default instead"
+                testRunner = InjectTestRunnerFactory.getDefault(currentBehaviour, type)
+                if (testRunner) {
+                    testRunner.initialize()
+                }
+            }
+        }
     }
 
     /**
      * Called when a Story or Specification file after is executed.
      * Publishes the event testCaseEnd.
      */
+    @Override
     public void stopBehavior(BehaviorStep behaviorStep, Behavior behavior) {
         super.stopBehavior(behaviorStep, behavior)
         eventPublisher.testCaseEnd(trucateEventName(behavior.phrase))
+
+        currentBehaviour = null
+        testRunner = null
     }
 
     /**
      * Called when a Step is about to be executed. A Step is a closure in a easyb file.
      * Publishes the event testStart.
      */
+    @Override
     public synchronized void startStep(BehaviorStep behaviorStep) {
         super.startStep(behaviorStep)
         currentStep = behaviorStep
         eventPublisher.testStart(trucateEventName(behaviorStep.name))
+
+        steps.push(step)
+        currentStep = step
+
+        switch ( step.getStepType() ) {
+            case BehaviorStepType.EXECUTE: {
+                currentBehaviour.binding.grailsTest = { String name, style = null ->
+                    dynamicallyInjectGrailsTest( style, name );
+                }
+
+                if(testRunner) {
+                    testRunner.injectMethods(currentBehaviour.binding)
+                }
+                break
+            }
+            case BehaviorStepType.IT:
+            case BehaviorStepType.SCENARIO: {
+                if(testRunner) {
+                    testRunner.setUp()
+                }
+                break
+            }
+        }
+    }
+
+    @Override
+    public void stopStep() {
+        BehaviorStep step = steps.pop()
+
+        switch(step.getStepType()) {
+            case BehaviorStepType.EXECUTE: break
+            case BehaviorStepType.IT:
+            case BehaviorStepType.SCENARIO: {
+                if(testRunner != null) {
+                    testRunner.tearDown()
+                }
+                break
+            }
+        }
     }
 
     /**
      * Called when a Step has been executed and has a result.
      * Publishes the event testEnd, and also the event testFailure if the result is marked as failed.
      */
+    @Override
     public void gotResult(Result result) {
         super.gotResult(result)
 
@@ -72,7 +142,8 @@ public class GrailsEasybListener extends ResultsCollector {
      * Called when all easyb files have been executed.
      * Generates the easyb reports.
      */
-    void completeTesting() {
+    @Override
+    public void completeTesting() {
         super.completeTesting()
         reportsFactory.produceReports(this)
     }
@@ -89,6 +160,24 @@ public class GrailsEasybListener extends ResultsCollector {
             return name
         } else {
             return name.substring(0, delimitator)
+        }
+    }
+
+    /**
+    * we can have situations where we want to replace the test case attached. This happens in the middle of a scenario or specification
+    * so we need to make sure we tell the new test runner about the binding
+    */
+    private void dynamicallyInjectGrailsTest(String name, String style) {
+        testRunner = InjectTestRunnerFactory.findDynamicRunner(style, name, currentBehaviour, type, testHelper)
+
+        if(testRunner) {
+            testRunner.initialize()
+            testRunner.injectMethods(currentBehaviour.binding)
+
+            // we have missed the start of the scenario or specification as well, so we need to inject the setup
+            if(currentStep.stepType == BehaviorStepType.IT || BehaviorStepType.SCENARIO) {
+                testRunner.setUp()
+            }
         }
     }
 }
