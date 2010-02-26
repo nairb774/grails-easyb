cf6fcd8e42b915c24b5568fbf5b8be9ab4258948
Gustavo Madruga <omadruga@gmail.com>
2010-02-26 12:08:53 -0300

    fixed plugin descriptor to generate correct zip file, so now don't need eventPackagePluginEnd to fix zip neither plugin-correct.xml. Also updated info about authors, etc.

--- EasybGrailsPlugin.groovy
+++ EasybGrailsPlugin.groovy
@@ -3,25 +3,20 @@ class EasybGrailsPlugin {
     // so it won't even compile any longer
     def version = "2.0"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.2.0.RC1 > *"
+    def grailsVersion = "1.2.0 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
-            "grails-app/conf/*",
-            "grails-app/domain/**",
-            "grails-app/controllers/**",
-            "test/*",
-            "web-app/**",
-            "grails-app/views/**"
+        'grails-app/**',
+        'test/**',
+        'web-app/**'         
     ]
-    def environments = ['test']
     def scopes = [excludes:'war']
 
     // inject code by Jeffrey Erikson
-
-    def author = "Richard Vowles, Gustavo Madruga"
-    def authorEmail = "richard@bluetrainsoftware.com"
+    def author = "Richard Vowles, Gustavo Madruga, Jeffrey Erikson"
+    def authorEmail = "richard@bluetrainsoftware.com, omadruga@gmail.com"
     def title = "Allows Grails applications to write unit, integration and functional tests. Integrates Grails unit testing features"
     def description = '''\\
 Groovy testing implementation of easyb - exposes all of the easyb functionality and an infrastructure to expose more testing functionality.
@@ -30,21 +25,10 @@ Groovy testing implementation of easyb - exposes all of the easyb functionality 
     // URL to the plugin's documentation
     def documentation = "http://grails.org/plugin/easyb"
 
-    def doWithSpring = {
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-    }
-
-    def doWithWebDescriptor = { xml ->
-    }
-
-    def doWithDynamicMethods = { ctx ->
-    }
-
-    def onChange = { event ->
-    }
-
-    def onConfigChange = { event ->
-    }
+    def doWithSpring = { }
+    def doWithApplicationContext = { applicationContext -> }
+    def doWithWebDescriptor = { xml -> }
+    def doWithDynamicMethods = { ctx -> }
+    def onChange = { event -> }
+    def onConfigChange = { event -> }
 }
